0c0de0c537b7a548f025bd2f636f96b5b650fc47
santiemanuel <santiemanuel@gmail.com>
2024-04-11 18:25:22 -0300

    Curso con imágenes

--- proyecto_cursos/settings.py
+++ proyecto_cursos/settings.py
@@ -123,6 +123,12 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
+STATIC_ROOT = "staticfiles/"
+
+# Archivos de Medios
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
